a1ef14587872a4c713c23c1b1f9ec5c087e647ae
Otmane Aatik <aaotmane@gmail.com>
2023-03-10 10:40:08 -0500

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+  
+  );
+}
+
+export default Button
